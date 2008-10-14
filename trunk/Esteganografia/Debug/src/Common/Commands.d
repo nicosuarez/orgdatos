f43db8f6bffb50c63ef053a330de1fc9e44b5dd0
scamjayi <scamjayi@a45b7b46-6a48-0410-bd0a-d9df811b1ca7>
2008-10-14 01:35:04 +0000

    Elimino la carpeta debug no tiene sentido tenerla arriba

--- trunk/Esteganografia/Debug/src/Common/Commands.d
+++
@@ -1,4 +0,0 @@
-src/Common/Commands.d src/Common/Commands.o: ../src/Common/Commands.cpp \
-  ../src/Common/Commands.h
-
-../src/Common/Commands.h:
--- trunk/Esteganografia/Debug/src/Common/ImageManager.d
+++
@@ -1,4 +0,0 @@
-src/Common/ImageManager.d src/Common/ImageManager.o:  \
- ../src/Common/ImageManager.cpp ../src/Common/ImageManager.h
-
-../src/Common/ImageManager.h:
