a94a430fcfc78fc08a2f773c32b6b7cc2bae6d6d
nicolas.suarez@gmail.com <nicolas.suarez@gmail.com@a45b7b46-6a48-0410-bd0a-d9df811b1ca7>
2009-05-17 17:28:24 +0000

    SetManager anda

--- trunk/wiki_sets/Common/SetsManager.h
+++ trunk/wiki_sets/Common/SetsManager.h
@@ -9,11 +9,13 @@
 #include "Constants.h"
 #include <list>
 #include <glibmm/ustring.h>
+#include <fstream>
 #include "../DataAccess/Organizations/OrgExtensibleRelative.h"
 #include "../DataAccess/Registries/SetRegistry.h"
 
 using Glib::ustring;
 using std::list;
+using std::fstream;
 
 class SetsManager {
 public:
@@ -28,17 +30,21 @@ public:
 	ID_type addSet();
 
 	/*Retorna la lista de palabras de un set*/
-	list<ustring>* getSet(ID_type idSet)const;
+	list<ustring>* getSet(ID_type idSet);
 
 private:
 
 	/*Constructor privado*/
 	SetsManager();
 
+	list<ustring>* getList (long int offset, string &cant, ifstream &arch);
+
 	/*Puntero a la unica instancia del SetManager*/
 	static SetsManager *instance;
 
 	OrgExtensibleRelative orgSets;
+
+//	fstream arch;
 
 	int count;
 
